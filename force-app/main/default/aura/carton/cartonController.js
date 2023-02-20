300d30b3fc84b42a534174159f62362d2ef7cb36
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-02-20 09:07:58 +0100

    code comment mode on

--- force-app/main/default/aura/carton/cartonController.js
+++ force-app/main/default/aura/carton/cartonController.js
@@ -1,6 +1,8 @@
 ({
     comparar : function(component, event, helper) {
+        // obtengo el numero aleatorio
         var bola = event.getParam("bola");
+        // obtengo el carton
         var carton = component.find("carton").getElement();
         var contador1 = 0;
         var contador2 = 0;
@@ -8,19 +10,21 @@
         var linea = document.getElementById("linea");
         var dobleLinea = document.getElementById("dobleLinea");
         var bingo = document.getElementById("bingo");
-
+        // recorro el carton y si el numero aleatorio coincide con alguno del carton lo pongo en X
         for (var i = 0; i < carton.getElementsByTagName("tr").length; i++) {
             for (var j = 0; j < carton.getElementsByTagName("tr")[i].getElementsByTagName("td").length; j++) {
-
+                // si el numero aleatorio coincide con alguno del carton lo pongo en X
                 if(carton.getElementsByTagName("tr")[i].getElementsByTagName("td")[j].innerHTML == bola)
                     carton.getElementsByTagName("tr")[i].getElementsByTagName("td")[j].innerHTML = "X"
             }
         }
+        // recorro el carton y si hay 5 X en una linea pongo el input de linea en visible
         for (var j = 0; j < carton.getElementsByTagName("tr")[0].getElementsByTagName("td").length; j++){
             if(carton.getElementsByTagName("tr")[0].getElementsByTagName("td")[j].innerHTML == "X"){
                contador1++; 
             }
         }
+        // si hay 5 X en una linea pongo el input de linea en visible
         if(contador1 == 5){
             document.getElementById('linea').value = 'LINEA';
             linea.type= "text";
@@ -45,12 +49,12 @@
             document.getElementById('linea').value = 'LINEA';
             linea.type= "text";
         }
-
+        // si hay 5 X en dos lineas pongo el input de doble linea en visible
         if(contador1 == 5 && contador2 == 5 || contador1 == 5 && contador3 == 5){
             document.getElementById('dobleLinea').value = 'DOBLE LINEA';
             dobleLinea.type= "text";
         }
-
+        // si hay 5 X en tres lineas pongo el input de bingo en visible
         if(contador1 == 5 && contador2 == 5 && contador3 == 5){
             document.getElementById('bingo').value = 'BINGO';
             bingo.type= "text";
--- force-app/main/default/aura/sacarBola/sacarBolaController.js
+++ force-app/main/default/aura/sacarBola/sacarBolaController.js
@@ -2,17 +2,24 @@
     sacarBola : function(component, event, helper) {
         var action = component.get("c.numeroAleatorio");
         action.setCallback(this, function(response) {
-            
+            // obtengo el estado de la respuesta
             var state = response.getState();
+            // si el estado es SUCCESS
             if (state === "SUCCESS") {
+                // devuelve un numero aleatorio entre 1 y 100
                 var bola = response.getReturnValue();
+                // se lo asigno a la variable bola del componente
                 component.find("bola").getElement().value = bola;
+                // creo un evento y le paso el numero aleatorio
                 var appevent =$A.get("e.c:Evento");
+                // le paso el numero aleatorio
                 appevent.setParams({"bola":bola});
+                // dispara el evento
                 appevent.fire();
             }
         
         });
+        // sin esto no funciona y se pone al final del todo
         $A.enqueueAction(action);
         
 
